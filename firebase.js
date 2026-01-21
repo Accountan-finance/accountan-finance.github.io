0ec3df65b3c9ebbc3d8cd00f20e2a5422e8aabe0
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-21 15:42:37 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 
-// js/firebase.js
-
+// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyCtnXY6BcQ0YmOS3E_SFj0BLnzb4-ISe2c",
   authDomain: "accountan-finance.firebaseapp.com",
@@ -10,9 +9,5 @@ const firebaseConfig = {
   appId: "1:1057932521410:web:19183a86b5a4721db2f05b"
 };
 
-// INIT – FAQAT 1 MARTA
 firebase.initializeApp(firebaseConfig);
-
-// GLOBAL EXPORT
-window.auth = firebase.auth();
-window.db   = firebase.firestore();
+const auth = firebase.auth();
