174a385d0d0606285971c31242e10d4c995a2880
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-21 14:45:07 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,6 +12,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
 
 // Global services
 const auth = firebase.auth();
