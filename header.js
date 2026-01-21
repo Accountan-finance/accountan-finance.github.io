6a721d1a4cd45a90b0611e9de33498b335898aa6
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-21 14:57:44 +0500

    Update header.js

--- header.js
+++ header.js
@@ -10,3 +10,18 @@ auth.onAuthStateChanged(user => {
     btn.href = "login.html";
   }
 });
+
+auth.onAuthStateChanged(user => {
+  const loginLink = document.getElementById("login-link");
+  const profileLink = document.getElementById("profile-link");
+
+  if (!loginLink || !profileLink) return;
+
+  if (user) {
+    loginLink.style.display = "none";
+    profileLink.style.display = "inline-block";
+  } else {
+    loginLink.style.display = "inline-block";
+    profileLink.style.display = "none";
+  }
+});
