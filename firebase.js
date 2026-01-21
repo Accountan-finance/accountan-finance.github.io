5e5aa80fac5987cde968427e05de5785452ea503
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-21 16:38:09 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,8 @@
-// firebase.js (FAOL, TOZA, TO‘G‘RI)
+// js/firebase.js
+
+import "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js";
+import "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js";
+import "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCtnXY6BcQ0YmOS3E_SFj0BLnzb4-ISe2c",
@@ -9,9 +13,7 @@ const firebaseConfig = {
   appId: "1:1057932521410:web:19183a86b5a4721db2f05b"
 };
 
-// INIT — FAQAT BIR MARTA
 firebase.initializeApp(firebaseConfig);
 
-// GLOBAL QILIB QO‘YAMIZ
 window.auth = firebase.auth();
-window.db = firebase.firestore?.();
+window.db = firebase.firestore();
