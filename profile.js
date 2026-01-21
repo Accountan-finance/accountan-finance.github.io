4d3d0b8bfff927e338b97c620bb966e447591273
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-21 14:44:21 +0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,17 +1,3 @@
-// 🔐 Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyCtnXY6BcQ0YmOS3E_SFj0BLnzb4-ISe2c",
-  authDomain: "accountan-finance.firebaseapp.com",
-  projectId: "accountan-finance",
-  storageBucket: "accountan-finance.firebasestorage.app",
-  messagingSenderId: "1057932521410",
-  appId: "1:1057932521410:web:19183a86b5a4721db2f05b"
-};
-
-// INIT
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
 
 // 🔑 LOGIN
 function login() {
