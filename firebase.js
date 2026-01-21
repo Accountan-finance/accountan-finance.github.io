d51b1991674e29fd87e4e3784621a9a5f260a2f8
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-21 15:25:51 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,18 @@
-<!-- firebase.js -->
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js"></script>
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyCtnXY6BcQ0YmOS3E_SFj0BLnzb4-ISe2c",
-    authDomain: "accountan-finance.firebaseapp.com",
-    projectId: "accountan-finance",
-    storageBucket: "accountan-finance.appspot.com",
-    messagingSenderId: "1057932521410",
-    appId: "1:1057932521410:web:19183a86b5a4721db2f05b"
-  };
+// js/firebase.js
 
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyCtnXY6BcQ0YmOS3E_SFj0BLnzb4-ISe2c",
+  authDomain: "accountan-finance.firebaseapp.com",
+  projectId: "accountan-finance",
+  storageBucket: "accountan-finance.firebasestorage.app",
+  messagingSenderId: "1057932521410",
+  appId: "1:1057932521410:web:19183a86b5a4721db2f05b"
+};
 
-  window.auth = firebase.auth();
-  window.db   = firebase.firestore();
-</script>
+// INIT – FAQAT 1 MARTA
+firebase.initializeApp(firebaseConfig);
+
+// GLOBAL EXPORT
+window.auth = firebase.auth();
+window.db   = firebase.firestore();
