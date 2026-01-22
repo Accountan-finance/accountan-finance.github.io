90232bdabfa468668978f988fb25743d1d9aeb9d
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-22 22:40:27 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,9 @@
-// auth.js
 import { auth, db } from "./firebase.js";
-
 import {
   GoogleAuthProvider,
-  signInWithRedirect
+  signInWithPopup,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
 import {
   doc,
   setDoc,
@@ -16,6 +14,27 @@ const provider = new GoogleAuthProvider();
 
 const loginBtn = document.getElementById("googleLogin");
 
-loginBtn.addEventListener("click", async () => {
-  await signInWithRedirect(auth, provider);
-});
+if (loginBtn) {
+  loginBtn.addEventListener("click", async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // user Firestore ga yoziladi (birinchi marta bo‘lsa)
+      await setDoc(
+        doc(db, "users", user.uid),
+        {
+          email: user.email,
+          name: user.displayName,
+          createdAt: serverTimestamp()
+        },
+        { merge: true }
+      );
+
+      window.location.href = "profile.html";
+    } catch (err) {
+      alert("Kirishda xatolik: " + err.message);
+      console.error(err);
+    }
+  });
+}
