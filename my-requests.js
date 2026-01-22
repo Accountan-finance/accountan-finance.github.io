5800df5f669536acd0b357f7de3e0cc862aff8e5
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-22 22:41:16 +0500

    Update my-requests.js

--- my-requests.js
+++ my-requests.js
@@ -1,3 +1,13 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
+
 import { auth, db } from "./firebase.js";
 import {
   collection,
