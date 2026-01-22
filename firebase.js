8df28c42994d0276c3923efd3ee41f659de29ac9
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-22 16:00:08 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey:  "AIzaSyCtnXY6BcQ0YmOS3E_SFj0BLnzb4-ISe2c",
+  apiKey: "AIzaSyCtnXY6BcQ0YmOS3E_SFj0BLnzb4-ISe2c",
   authDomain: "accountan-finance.firebaseapp.com",
   projectId: "accountan-finance",
   storageBucket: "accountan-finance.appspot.com",
@@ -12,5 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 
+export const auth = getAuth(app);
+export const db = getFirestore(app); // 🔥 ENG MUHIM QATOR
