cb0abe1b8776773250f8a652767ee32d01cd0b7d
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-23 16:44:47 +0500

    Update support.js

--- support.js
+++ support.js
@@ -32,3 +32,19 @@ onAuthStateChanged(auth, (user) => {
     }
   });
 });
+
+async function notifyTelegram(text, email) {
+  const TOKEN = "8444694860:AAHCOKSRgS7oSQQo8FysQSogt1B4V_PN70k";
+  const CHAT_ID = "1736401983";
+
+  const msg = `🆕 Yangi murojaat\n👤 ${email}\n\n💬 ${text}`;
+
+  await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: msg
+    })
+  });
+}
