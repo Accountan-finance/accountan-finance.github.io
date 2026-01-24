31ac0cbf9efe31f562eb4a3bc86196a8f8b6c57b
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-24 13:47:11 +0500

    Update support.js

--- support.js
+++ support.js
@@ -1,6 +1,8 @@
 import { auth, db } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { doc, getDoc } from 
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const btn = document.getElementById("sendBtn");
 const textarea = document.getElementById("message");
