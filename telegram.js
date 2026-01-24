72b642c3caf51da22fb21cf7d241746b0cddc3f2
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-24 12:36:03 +0500

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,14 +1,18 @@
+
 // telegram.js
+const BOT_TOKEN = "8444694860:AAHCOKSRgS7oSQQo8FysQSogt1B4V_PN70k";
+const CHAT_ID = "1736401983";
+
 export function sendToTelegram(text) {
-  const BOT_TOKEN = "8444694860:AAHCOKSRgS7oSQQo8FysQSogt1B4V_PN70k";
-  const CHAT_ID = "1736401983";
+  if (!text) return;
 
   const url =
     `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage` +
     `?chat_id=${CHAT_ID}` +
+    `&parse_mode=HTML` +
     `&text=${encodeURIComponent(text)}`;
 
-  // 👇 ENG MUHIM JOY (CORSsiz ishlaydi)
+  // CORS workaround
   const img = new Image();
   img.src = url;
 }
