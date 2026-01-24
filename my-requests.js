26bcbe580b19476806b8eb2c7f05e69ac5b75a8c
Accountan-finance <boshqaishlaruch@gmail.com>
2026-01-24 13:03:36 +0500

    Update my-requests.js

--- my-requests.js
+++ my-requests.js
@@ -42,22 +42,7 @@ onAuthStateChanged(auth, (user) => {
           <div class="replies" id="replies-${id}"></div>
         </div>
       `;
-import { sendToTelegram } from "./telegram.js";
 
-await addDoc(collection(db, "support_requests"), {
-  userId: auth.currentUser.uid,
-  message: text,
-  createdAt: serverTimestamp(),
-  status: "new"
-});
-
-// 🔔 TELEGRAMGA XABAR
-sendToTelegram(
-  `<b>📩 Yangi murojaat</b>\n\n` +
-  `👤 User: ${auth.currentUser.email}\n` +
-  `📝 Matn: ${text}`
-);
-      
       // admin javoblari
       onSnapshot(
         query(
